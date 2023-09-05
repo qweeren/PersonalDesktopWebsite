9c570a881eba49c44f95373f26c3a23f0f5688f9
Eren <erenyordem2006@gmail.com>
2023-09-05 19:08:28 +0300

    selection fix

--- src/lib/desktop/desktop.svelte
+++ src/lib/desktop/desktop.svelte
@@ -1,77 +1,75 @@
 <script>
-  import GoogleProgram from "../apps/Google/GoogleProgram.svelte";
-  import Mapsprogram from "../apps/Maps/Mapsprogram.svelte";
-import Cvpage from "../apps/cv/cvpage.svelte";
-  import Cvprogram from "../apps/cv/cvprogram.svelte";
-  import Gamesprogram from "../apps/doom/gamesprogram.svelte";
-  import Doomprogram from "../apps/doom/gamesprogram.svelte";
-    import SnakePage from "../apps/snake/SnakePage.svelte";
-import SnakeProgram from "../apps/snake/SnakeProgram.svelte";
+    import GoogleProgram from "../apps/Google/GoogleProgram.svelte";
+    import Mapsprogram from "../apps/Maps/Mapsprogram.svelte";
+    import Cvprogram from "../apps/cv/cvprogram.svelte";
+    import Gamesprogram from "../apps/doom/gamesprogram.svelte";
+    import SnakeProgram from "../apps/snake/SnakeProgram.svelte";
     import Shortcuts from "./shortcuts.svelte";
     import { onMount } from "svelte";
 
     let desktop;
+    let desktopGrid;
+  let rectangle;
+  let isResizing = false;
+  let initialX;
+  let initialY;
+  let selectionWidth;
+  let selectionHeight;
 
-    let rectangle;
-    let isResizing = false;
-    let initialX;
-    let initialY;
-    let selectionwidth;
-    let selectionheight;
+  function handleMouseDown(event) {
+    if (event.target === desktop || event.target === desktopGrid) {
+      isResizing = true;
+      initialX = event.clientX;
+      initialY = event.clientY;
 
-    function handleMouseDown(event) {
-        if(event.target.className != this.className){
+      rectangle.style.width = "0";
+      rectangle.style.height = "0";
+      rectangle.style.left = `${initialX}px`;
+      rectangle.style.top = `${initialY}px`;
+    }
+  }
 
-        isResizing = true;
-        initialX = event.clientX;
-        initialY = event.clientY;
+  function handleMouseMove(event) {
+    if (!isResizing) return;
 
-        rectangle.style.selectionwidth = "0";
-        rectangle.style.selectionheight = "0";
-        rectangle.style.left = `${initialX}px`;
-        rectangle.style.top = `${initialY}px`;
-        }
+    const currentX = event.clientX;
+    const currentY = event.clientY;
+
+    selectionWidth = Math.abs(currentX - initialX);
+    selectionHeight = Math.abs(currentY - initialY);
+
+    rectangle.style.width = `${selectionWidth}px`;
+    rectangle.style.height = `${selectionHeight}px`;
+
+    if (currentX < initialX) {
+      rectangle.style.left = `${currentX}px`;
     }
 
-    function handleMouseMove(event) {
-        if (!isResizing) return;
-
-        const currentX = event.clientX;
-        const currentY = event.clientY;
-
-        selectionwidth = Math.abs(currentX - initialX);
-        selectionheight = Math.abs(currentY - initialY);
-
-        rectangle.style.selectionwidth = `${selectionwidth}px`;
-        rectangle.style.selectionheight = `${selectionheight}px`;
-
-        if (currentX < initialX) {
-        rectangle.style.left = `${currentX}px`;
-        }
-
-        if (currentY < initialY) {
-        rectangle.style.top = `${currentY}px`;
-        }
+    if (currentY < initialY) {
+      rectangle.style.top = `${currentY}px`;
     }
+  }
 
-    function handleMouseUp() {
-        isResizing = false;
-    }
+  function handleMouseUp() {
+    isResizing = false;
+  }
 
-    onMount(() => {
-        desktop = document.getElementById("desktop")
-        rectangle = document.querySelector(".selection");
-        desktop.addEventListener("mousedown", handleMouseDown);
-        window.addEventListener("mousemove", handleMouseMove);
-        window.addEventListener("mouseup", handleMouseUp);
-    });
+  onMount(() => {
+    desktop = document.getElementById("desktop");
+    desktopGrid = document.getElementById("grid");
+    rectangle = document.querySelector(".selection");
+    desktop.addEventListener("mousedown", handleMouseDown);
+    desktopGrid.addEventListener("mousedown", handleMouseDown);
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+  });
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="desktop" id="desktop">
     <div class="selection" hidden={!isResizing}></div>
-    <div class="desktop-grid">
+    <div class="desktop-grid" id="grid">
         <Shortcuts/>
     </div>
     <div class="appenv">
