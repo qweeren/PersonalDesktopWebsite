1e54cb1eb5bc088688b7cac344e602b0cb9226fc
Eren <erenyordem2006@gmail.com>
2023-10-11 20:44:32 +0300

    changed default ms settings

--- src/lib/apps/minesweeper/msstore.js
+++ src/lib/apps/minesweeper/msstore.js
@@ -1,8 +1,8 @@
 import { writable } from 'svelte/store';
 
 export const settings = writable({
-    rows: 10,
-    cols: 15,
+    rows: 9,
+    cols: 10,
     safeZone: 4,
     cellSize: 30,
     mines: 30,
